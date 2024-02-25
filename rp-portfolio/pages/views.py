864ff2b50eccca8a8e3d15aede5e1688b4989dd1
coors-light <rcoors6337@gmail.com>
2024-02-25 08:55:04 -0500

    Get request changes

--- rp-portfolio/pages/views.py
+++ rp-portfolio/pages/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from .models import SearchPlant
+from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
+def inputPlant(request):
     if request.method == 'GET':
-        plants = request.GET['plantSearch']
+        plants = request.GET.get('plantSearch')
 
         new_plant = SearchPlant(newplant = plants)
         new_plant.save()
+        return HttpResponse('Received: ' + new_plant)
+
     text = 'does this work'
     return render(request, "pages/index.html", {'text': text})
